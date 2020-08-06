7f94fc1c08a7af8e9d5d5e9b0000d16ac708c6df
MMendes24 <mmbusiness1@protonmail.com>
2020-08-06 15:48:32 -0400

    Fixed an ID issue where the labels and input weren't associated. Inputs functioning.

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,24 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, fireEvent, screen } from "@testing-library/react";
 import App from "./App";
 
 test("renders App without crashing", () => {
 
   render(<App />);
-  
+
 });
+
+test('We can enter things into the input', () => {
+  //first define get functions as variables for convenience
+  render(<App />);
+  const fnInput = screen.getByLabelText(/first name/i)
+  const lnInput = screen.getByLabelText(/last name/i)
+  const emailInput = screen.getByLabelText(/email/i)
+  const mesInput = screen.getByLabelText(/message/i)
+
+  //next we fire events
+  fireEvent.change(fnInput, { target: { value: 'Lord'} })
+  fireEvent.change(lnInput, { target: { value: 'Buggycode III'} })
+  fireEvent.change(emailInput, { target: { value: 'lordbuggycode3@gmail.com'} })
+  fireEvent.change(mesInput, { target: { value: 'I have arrived, peasants!'} })
+})
